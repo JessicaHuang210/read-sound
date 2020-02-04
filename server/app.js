80a00b21fd0b555051ffa9179fad9c02da89219b
黃婉柔 <>
2020-02-04 21:59:17 +0800

    connect mongoDB cloud

--- server/app.js
+++ server/app.js
@@ -1,12 +1,18 @@
 const express = require("express");
+require("dotenv").config();
 var cors = require("cors");
 const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(cors());
-
-mongoose.connect("mongodb://localhost:27017/myapp", {
+// const oldDburl = "mongodb://localhost:27017/myapp"
+const dbUser = process.argv[2];
+const dbPsw = process.argv[3];
+console.log("dbUser", dbUser);
+console.log("dbPsw", dbPsw);
+const dbUrl = `mongodb+srv://${dbUser}:${dbPsw}@readsound-wvv8y.gcp.mongodb.net/test?retryWrites=true&w=majority`;
+mongoose.connect(dbUrl, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
--- src/pages/SearchSingers.js
+++ src/pages/SearchSingers.js
@@ -1,5 +1,4 @@
 import React, { Component, Fragment } from "react";
-import { H3 } from "components/Typography";
 import SearchSongs from "./SearchSongs";
 import Table from "components/Table";
 
