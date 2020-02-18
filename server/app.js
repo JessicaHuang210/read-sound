627317cf9cea23cabf932155d7aa2ba62e6eaee9
黃婉柔 <>
2020-02-18 23:10:52 +0800

    改變資料結構

--- server/app.js
+++ server/app.js
@@ -1,22 +1,21 @@
 const express = require("express");
 const path = require("path");
-const publicPath = path.join(__dirname, "..", "public");
+
 require("dotenv").config();
 var cors = require("cors");
 const mongoose = require("mongoose");
 
 const app = express();
-app.use(express.static(publicPath));
+app.use(express.static(path.join(__dirname, "..", "client/build")));
 
 app.use(cors());
 // const oldDburl = "mongodb://localhost:27017/myapp"
-const dbUser = process.argv[2];
-const dbPsw = process.argv[3];
-console.log("dbUser", dbUser);
+
+const dbPsw = process.argv[2];
 console.log("dbPsw", dbPsw);
-const dbUrl =
-  process.env.MONGODB_URL ||
-  `mongodb://${dbUser}:${dbPsw}@readsound-shard-00-00-wvv8y.gcp.mongodb.net:27017,readsound-shard-00-01-wvv8y.gcp.mongodb.net:27017,readsound-shard-00-02-wvv8y.gcp.mongodb.net:27017/test?ssl=true&replicaSet=readSound-shard-0&authSource=admin&retryWrites=true&w=majority`;
+console.log(process.env.PSW);
+const dbUrl = `mongodb://jess:${process.env.PSW ||
+  dbPsw}@readsound-shard-00-00-wvv8y.gcp.mongodb.net:27017,readsound-shard-00-01-wvv8y.gcp.mongodb.net:27017,readsound-shard-00-02-wvv8y.gcp.mongodb.net:27017/test?ssl=true&replicaSet=readSound-shard-0&authSource=admin&retryWrites=true&w=majority`;
 mongoose.connect(dbUrl, {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -31,8 +30,8 @@ mongoose.connection.once("open", (err, res) => {
 app.use(express.json());
 app.use("/getSongs", require("./routers/songs"));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(publicPath, "index.html"));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(process.env.PORT || 1313, () => console.log("runing on port 1313"));
