e3c42386162bae65d06a08bfc814d90192fdd881
Jessica <jessica.huang@silkrode.com.tw>
2020-02-19 18:04:12 +0800

    改成useState方法～

--- client/src/pages/SearchSongs.js
+++ client/src/pages/SearchSongs.js
@@ -1,37 +1,27 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
+import React, { useState } from "react";
 import InputText from "components/InputText";
 import { FormWrap, FormItem } from "components/Form";
 
-class SearchSongs extends Component {
-  static propTypes = {
-    searchKey: PropTypes.string,
-    onSearchSong: PropTypes.func,
-    placeholder: PropTypes.string
-  };
-  static defaultProps = {
-    searchKey: "name",
-    placeholder: "搜尋歌名..."
-  };
-  render() {
-    return (
-      <FormWrap>
-        <FormItem>
-          <InputText
-            type="search"
-            value={this.props.keywords}
-            name="keywords"
-            placeholder={this.props.placeholder}
-            onChange={e =>
-              this.props.onSearchSong({
-                [this.props.searchKey]: e.target.value.trim()
-              })
-            }
-          />
-        </FormItem>
-      </FormWrap>
-    );
-  }
+function SearchSongs(props) {
+  const [searchKey] = useState(props.name || "name");
+  const [placeholder] = useState(props.placeholder || "搜尋歌名...");
+  return (
+    <FormWrap>
+      <FormItem>
+        <InputText
+          type="search"
+          value={props.keywords}
+          name="keywords"
+          placeholder={placeholder}
+          onChange={e =>
+            props.onSearchSong({
+              [searchKey]: e.target.value.trim()
+            })
+          }
+        />
+      </FormItem>
+    </FormWrap>
+  );
 }
 
 export default SearchSongs;
